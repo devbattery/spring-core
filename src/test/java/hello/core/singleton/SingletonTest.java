840eb70e7d78e17119cf9dfeedb3cc07f63e09eb
Sully <2dcoder@naver.com>
2024-03-07 23:00:54 +0900

    섹션 4: 싱글톤 컨테이너
    싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class SingletonTest {
 
@@ -14,7 +16,7 @@ class SingletonTest {
     void pure_container() {
         // given
         AppConfig appConfig = new AppConfig();
-        
+
         // when
         MemberService memberService1 = appConfig.memberService();
         MemberService memberService2 = appConfig.memberService();
@@ -39,6 +41,24 @@ class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @DisplayName("스프링 컨테이너와 싱글톤 확인")
+    @Test
+    void spring_container() {
+        // given
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // when
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // then
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 
 }
