311f1bdbf8b00c349697acbc00898de5e34ac4d4
Sully <2dcoder@naver.com>
2024-03-08 19:28:32 +0900

    섹션 4: 싱글톤 컨테이너
    싱글톤 방식의 주의점

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,44 @@
+package hello.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+class StatefulServiceTest {
+
+    @DisplayName("")
+    @Test
+    void statefulServiceSingleton() {
+        // given
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // when
+        // Thread A: A 사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        // Thread B: B 사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        // Thread A: A 사용자 주문 금액 조회
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+
+        // then
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+
+    }
+
+}
